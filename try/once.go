bcb54c6b5c78c30ae396432392b4440f50348a95
Takuya Ueda <uedatakuya@gmail.com>
2018-06-25 08:17:53 +0900

    Fix try racecondtion

--- try/once.go
+++ try/once.go
@@ -2,21 +2,45 @@ package try
 
 import (
 	"sync"
+	"sync/atomic"
 )
 
 // Once is a type which added Try method to sync.Once.
 type Once struct {
-	sync.Once
+	m    sync.Mutex
+	done uint32
+}
+
+// Do is same behavior with sync.Once.Do.
+func (o *Once) Do(f func()) {
+	if atomic.LoadUint32(&o.done) == 1 {
+		return
+	}
+	// Slow-path.
+	o.m.Lock()
+	defer o.m.Unlock()
+	if o.done == 0 {
+		defer atomic.StoreUint32(&o.done, 1)
+		f()
+	}
 }
 
 // Try can call sync.Once's Do method with error.
 // If f returns non nil error it can retry.
 func (o *Once) Try(f func() error) (err error) {
-	o.Do(func() {
-		err = f()
-	})
-	if err != nil {
-		o.Once = sync.Once{}
+	if atomic.LoadUint32(&o.done) == 1 {
+		return nil
 	}
-	return err
+	// Slow-path.
+	o.m.Lock()
+	defer o.m.Unlock()
+	if o.done == 0 {
+		defer func() {
+			if err == nil {
+				atomic.StoreUint32(&o.done, 1)
+			}
+		}()
+		err = f()
+	}
+	return
 }
