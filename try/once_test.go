fe39b47ba4782884cd80905af4fc3308e6425341
Takuya Ueda <uedatakuya@gmail.com>
2018-06-25 07:17:36 +0900

    Add Try example test

--- try/once_test.go
+++ try/once_test.go
@@ -2,6 +2,7 @@ package try
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 )
 
@@ -64,4 +65,26 @@ func TestOnce(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Example_Once_Try() {
+	var once Once
+	for i := 1; i <= 3; i++ {
+		i := i
+		err := once.Try(func() error {
+			if i < 3 {
+				return errors.New("error")
+			}
+			return nil
+		})
+		if err != nil {
+			fmt.Printf("try %d %v\n", i, err)
+		} else {
+			fmt.Printf("try %d success\n", i)
+		}
+	}
+	// Output:
+	// try 1 error
+	// try 2 error
+	// try 3 success
 }
